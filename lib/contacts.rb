056b8ec3e929f0b235437b5dde4dbe7d8825cd75
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-21 13:19:21 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts[:Freddy Mercury]
+  no_strawberry = contacts["Freddy Mercury"][:favorite_ice_cream_flavors].shift
   binding.pry 
  end
 end
